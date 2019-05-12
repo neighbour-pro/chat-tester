89e6aa92394702cf08c40833c020d2f64ce8d00c
Raul Contreras <raulc98@gmail.com>
2019-05-12 16:20:44 +0200

    Created basic components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import "./App.css";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import ConversationsList from "./components/ConversationsList";
+
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
       <Switch>
-        <Route exact path="/login" render={()=> <p>Login</p>}/>
-        <Route exact path="/signup" render={()=> <p>Signup</p>}/>
-        <Route exact path="/conversations" render={()=> <p>Conversations list</p>}/>
+        <Route exact path="/login" render={()=> <Login/>}/>
+        <Route exact path="/signup" render={()=> <Signup/>}/>
+        <Route exact path="/conversations" render={()=> <ConversationsList/>}/>
         <Route render={()=> <Redirect to="/login"/>}/>
       </Switch>
     </React.Fragment>
---
+++ src/components/ConversationsList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ConversationsList extends Component {
+  render() {
+    return (
+      <div>
+        Lista de conversaciones
+      </div>
+    )
+  }
+}
